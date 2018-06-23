97c6cd525827530e3a26e497dd42f4b22f349370
Konrad Bartczak <konradbartczak@onet.pl>
2018-06-23 12:25:04 +0200

    Code fixed, added fields; telNumber, mobilePhoneNumber, mail.

--- school-commons/src/main/java/pl/sda/model/Parents.java
+++ school-commons/src/main/java/pl/sda/model/Parents.java
@@ -3,30 +3,11 @@ package main.java.pl.sda.model;
 public class Parents {
     private int id;
     private String surname;
-    private String student_id;
+    private String firstName;
+    private String studentId;
+    private String telNumber;
+    private String mobilePhoneNumber;
+    private String mail;
 
-    public int getId() {
-        return id;
-    }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public String getStudent_id() {
-        return student_id;
-    }
-
-    public void setStudent_id(String student_id) {
-        this.student_id = student_id;
-    }
 }
-
