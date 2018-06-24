ca8e49d0bbf7c456fd31092da5671fbe8b2b61ed
mborkowski13 <m.borkowski13@gmail.com>
2018-06-24 14:48:57 +0200

    fix to grade file

--- school-commons/src/main/java/pl/sda/model/Grade.java
+++ school-commons/src/main/java/pl/sda/model/Grade.java
@@ -1,53 +1,15 @@
 package pl.sda.model;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
+@Data
+@AllArgsConstructor
 public class Grade {
     private int id;
     private int subjectId;
     private int studentGradeId;
     private int grade;
 
-    public Grade(int id, int subjectId, int studentGradeId) {
-        this.id = id;
-        this.subjectId = subjectId;
-        this.studentGradeId = studentGradeId;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public int getSubjectId() {
-        return subjectId;
-    }
-
-    public void setSubjectId(int subjectId) {
-        this.subjectId = subjectId;
-    }
-
-    public int getStudentGradeId() {
-        return studentGradeId;
-    }
-
-    public void setStudentGradeId(int studentGradeId) {
-        this.studentGradeId = studentGradeId;
-    }
-
-    public int getGrade() { return grade;}
-
-    public void setGrade(int grade) {this.grade = grade; }
-
-    @Override
-    public String toString() {
-        return "Grade{" +
-                "id=" + id +
-                ", subjectId=" + subjectId +
-                ", studentGradeId=" + studentGradeId +
-                '}';
-    }
 }
