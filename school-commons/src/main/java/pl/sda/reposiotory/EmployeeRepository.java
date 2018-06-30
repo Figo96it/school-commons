37b5ccac63f73d883b08a4b5d78d393ba22e6d7f
Konrad Bartczak <konradbartczak@onet.pl>
2018-06-30 12:24:56 +0200

    (cherry picked from commit a7ecb08)

---
+++ school-commons/src/main/java/pl/sda/reposiotory/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package pl.sda.reposiotory;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import pl.sda.model.Employee;
+
+@Repository
+public interface EmployeeRepository extends CrudRepository<Employee, Integer> {
+}
